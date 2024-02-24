c84286df24186d425e014e062d001c2306fa8862
saad9930 <saad9930.sk@gmail.com>
2024-02-24 11:36:42 +0500

    first assingmentt

---
+++ main.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("hello world");
+console.log("my name saad and i m student of governor program");
---
+++ main.ts
@@ -0,0 +1,2 @@
+console.log("hello world")
+console.log("my name saad and i m student of governor program")
